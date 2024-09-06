656a7144db06bfc4804503da039d54979b5495ec
akramexp <akram.exp02@gmail.com>
2024-09-06 18:38:26 +0530

    added dark and light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div className="text-xl font-bold">
       <UserButton />
+      <ModeToggle />
     </div>
   );
 };
